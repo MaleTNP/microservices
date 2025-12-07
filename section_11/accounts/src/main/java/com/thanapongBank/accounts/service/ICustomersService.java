64970e7201b72a3fe79b5f611982b321f4f630c1
Thanapong Yooyen <malethanapong@gmail.com>
2025-12-07 22:31:14 +0700

    Making Observability and Monitoring in microservices

---
+++ section_11/accounts/src/main/java/com/thanapongBank/accounts/service/ICustomersService.java
@@ -0,0 +1,14 @@
+package com.thanapongBank.accounts.service;
+
+import com.thanapongBank.accounts.dto.CustomerDetailsDto;
+import com.thanapongBank.accounts.dto.CustomerDto;
+
+public interface ICustomersService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
+}
---
+++ section_11/accounts/src/main/java/com/thanapongBank/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,55 @@
+package com.thanapongBank.accounts.service.impl;
+
+import com.thanapongBank.accounts.dto.AccountsDto;
+import com.thanapongBank.accounts.dto.CardsDto;
+import com.thanapongBank.accounts.dto.CustomerDetailsDto;
+import com.thanapongBank.accounts.dto.LoansDto;
+import com.thanapongBank.accounts.entity.Accounts;
+import com.thanapongBank.accounts.entity.Customer;
+import com.thanapongBank.accounts.exception.ResourceNotFoundException;
+import com.thanapongBank.accounts.mapper.AccountsMapper;
+import com.thanapongBank.accounts.mapper.CustomerMapper;
+import com.thanapongBank.accounts.repository.AccountsRepository;
+import com.thanapongBank.accounts.repository.CustomerRepository;
+import com.thanapongBank.accounts.service.ICustomersService;
+import com.thanapongBank.accounts.service.client.CardsFeignClient;
+import com.thanapongBank.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Accounts", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoan(correlationId, mobileNumber);
+        if (null != loansDtoResponseEntity) {
+            customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        }
+//        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCard(correlationId, mobileNumber);
+        if (null != cardsDtoResponseEntity) {
+            customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        }
+//        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        return customerDetailsDto;
+    }
+}
